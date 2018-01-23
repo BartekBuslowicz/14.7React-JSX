c05b1159166c05fa529ae7ade377dae9160b1560
Bartek Buslowicz <bartek@burin.pl>
2018-01-23 21:45:06 +0100

    Add  ready3 project

--- components/Contact.js
+++ components/Contact.js
@@ -7,7 +7,7 @@ var Contact = React.createClass({
     return (
       <div className={'contactItem'}>
           <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
-          <P className={'contactLabel'}>iMIĘ: {this.props.item.firstName}</P>
+          <P className={'contactLabel'}>imię: {this.props.item.firstName}</P>
           <P className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</P>
           <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>{this.props.item.email}</a>
       </div>
