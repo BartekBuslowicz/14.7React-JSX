2010ebc443f8c700dacdc3a84997cd8df3025c53
Bartek Buslowicz <bartek@burin.pl>
2018-01-23 19:18:25 +0100

    Add  ready3 project

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,11 +1,12 @@
- Contacts = React.createClass({
+Contacts = React.createClass({
   propTypes: {
     items: React.PropTypes.array.isRequired,
   },
 
   render: function() {
     var contacts = this.props.items.map(function(contact) {
-        return <Contact item={contact} key={contact.id}></Contact>
+        return 
+          <Contact item={contact} key={contact.id}></Contact>
     });
 
     return (
