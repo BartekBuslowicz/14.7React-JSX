8ac730f8862c529d379b851edd4f42888803a4e2
Bartek Buslowicz <bartek@burin.pl>
2018-01-23 14:09:43 +0100

    Add  ready3 project

--- components/App.js
+++ components/App.js
@@ -29,7 +29,6 @@ var App = React.createClass({
   render: function() {
     return (
       <div className={'app'}>
-        <h2 className={'titleForm'}>Contact form</h2>
          <ContactForm contact={contactForm}></ContactForm>
       <Contacts items={contacts}></Contacts>
     </div>
