9d1667b873ae01f1397867600d4a9d8e5032b0f9
Bartek Buslowicz <bartek@burin.pl>
2018-01-23 14:15:55 +0100

    Add  ready3 project

--- components/App.js
+++ components/App.js
@@ -25,17 +25,12 @@ var contactForm = {
   email: ''
 };
 
-var App = React.createClass({
+ App = React.createClass({
   render: function() {
-    return (
-      <div className={'app'}>
-        <ContactForm 
-          contact="contactForm"
-        />
-        <Contacts 
-          items="contacts"
-        />
-      </div>
-    );
+    return( <div className={'app'}>
+      <h2 className={'titleForm'}>Contact form</h2>
+      <ContactForm contact={contactForm}></ContactForm>
+      <Contacts items={contacts}></Contacts>
+    </div>);
   }
 });
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -4,25 +4,16 @@ var ContactForm = React.createClass({
   },
 
   render: function() {
-    return (
-      React.createElement('form', {className: 'contactForm'},
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Imię',
-          value: this.props.contact.firstName,
-        }),
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Nazwisko',
-          value: this.props.contact.lastName,
-        }),
-        React.createElement('input', {
-          type: 'email',
-          placeholder: 'Email',
-          value: this.props.contact.email,
-        }),
-        React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
-      )
-    )
+    return ( <form className={'contactForm form-inline'}>
+              <input className={'form-control mb-2 mr-sm-2 mb-sm-0'} type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}></input>
+              <input className={'form-control mb-2 mr-sm-2 mb-sm-0'} type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}></input>
+                <div className={'input-group mb-2 mr-sm-2 mb-sm-0'}>
+                  <div className={'input-group-addon'}>@
+                    <input className={'form-control'} type={'email'} placeholder={'Email'} value={this.props.contact.email}></input>
+                  </div>
+                </div>  
+              <button className={'btn btn-primary'} type={'submit'}>Dodaj kontakt</button>
+            </form>
+          );
   },
 })
