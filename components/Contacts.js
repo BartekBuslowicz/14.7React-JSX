a443d948d6d71b80e3c0577b2e8d434d87a11001
Bartek Buslowicz <bartek@burin.pl>
2018-01-23 20:25:42 +0100

    Add  ready3 project

--- components/Contacts.js
+++ components/Contacts.js
@@ -6,7 +6,7 @@ var Contacts = React.createClass({
   render: function() {
     var contacts = this.props.items.map(function(contact) {
         return 
-          console.log('lala')
+          console.log('lala');
           <Contact item={contact} key={contact.id}></Contact>
     });
 
